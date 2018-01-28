32772325dc6ca565d529f6f1359832a1b4547160
Miquel De Caceres <miquelcaceres@gmail.com>
2018-01-28 20:00:50 +0100

    doc readxml

--- R/readVegXML.R
+++ R/readVegXML.R
@@ -1,3 +1,10 @@
+#' Reads a Veg-X XML file
+#'
+#' @param file the filename of an XML file following the Veg-X standard (ver. 2.0)
+#'
+#' @return An object of class \code{\linkS4class{VegX}}
+#' @export
+#' 
 readVegXML<-function(file) {
   target = newVegX()
   veg=xmlRoot(xmlTreeParse(file, useInternalNodes = T))
