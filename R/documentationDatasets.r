17c00b0fc136b6e0026b0023ebf3b381680f4694
Miquel De Caceres <miquelcaceres@gmail.com>
2018-01-31 16:34:39 +1300

    New dataset takitimu

--- R/documentationDatasets.r
+++ R/documentationDatasets.r
@@ -6,7 +6,7 @@
 #' \itemize{
 #'  \item moki_loc Location of vegetation plots (5 records x 25 variables)
 #'  \item moki_site Site characteristics of plots/subplots (25 records x 43 variables)
-#'  \item moki_tcv Observations of taxon categories, grouped by vegetation strata (578 records x 33 variables)
+#'  \item moki_tcv Observations of cover of taxon categories, grouped by vegetation strata (578 records x 33 variables)
 #'  \item moki_dia Observations made on individual trees (968 records x 33 variables)
 #' }
 #'
@@ -25,13 +25,35 @@ NULL
 #' \itemize{
 #'  \item mtfyffe_loc Location of vegetation plots (10 records x 25 variables)
 #'  \item mtfyffe_site Site characteristics of plots/subplots (325 records x 29 variables)
-#'  \item mtfyffe_counts Observations of taxon categories, grouped by vegetation strata (543 records x 30 variables)
+#'  \item mtfyffe_counts Observations of counts of taxon categories, grouped by vegetation strata (543 records x 30 variables)
 #'  \item mtfyffe_dia Observations made on individual trees (880 records x 31 variables)
 #'  \item mtfyffe_disturbance Observations of disturbance events on plots (9 records x 10 variables)
 #'  \item mtfyffe_groundcover Observations of ground cover on plots (50 records x 12 variables)
 #' }
 #'
 #' @name mtfyffe
+#' @docType data
+#' @author NVS database. Landcare Research (New Zealand) \email{nvs@landcareresearch.co.nz}
+#' @keywords data
+#' @references Wiser, S.K., P.J. Bellingham and L.E. Burrows.  2001.  Managing biodiversity information:  development of the National Vegetation Survey Databank.  New Zealand Journal of Ecology 25(2): 1-17.
+NULL
+
+
+
+#' Takitimu example dataset for Veg-X
+#'
+#' The data set concerns observations made on 5 plots with their subplots.
+#' The data set includes five data frames:
+#'
+#' \itemize{
+#'  \item taki_loc Location of vegetation plots (10 records x 25 variables)
+#'  \item taki_site Site characteristics of plots/subplots (325 records x 29 variables)
+#'  \item taki_freq Observations of frequency of taxon categories (543 records x 30 variables)
+#'  \item taki_disturbance Observations of disturbance events on plots (9 records x 10 variables)
+#'  \item taki_groundcover Observations of ground cover on plots (50 records x 12 variables)
+#' }
+#'
+#' @name takitimu
 #' @docType data
 #' @author NVS database. Landcare Research (New Zealand) \email{nvs@landcareresearch.co.nz}
 #' @keywords data
