8a47a9fc075e24693e02543743ddd480f90d5b1e
Miquel De Caceres <miquelcaceres@gmail.com>
2018-02-16 17:21:02 +0100

    update addTaxonBySiteData

--- R/addTaxonBySiteData.R
+++ R/addTaxonBySiteData.R
@@ -1,17 +1,12 @@
-#' Add a taxon-by-site table
+#' Add a taxon by site table
 #'
-#' Adds the aggregate organism observations of a taxon-by-site table to an existing VegX object.
-#' Data can be added to an existing project or a new project can be defined within the VegX object,
-#' depending on the input project title.
-#' Vegetation plots and taxon names can be the same as those already existing in the target VegX.
-#' Subplots cannot be defined with this function, but observations can be referred to them if they exist.
-#' New/old plot observations are distinguished by observation date.
+#' Adds the aggregate organism observations of a taxon by site table to an existing VegX object.
 #'
-#' @param target The object of class \code{\linkS4class{VegX}} to be modified
-#' @param x A site-by-species releve table
-#' @param projectTitle A character string to identify the project title, which can be the same as one of the currently defined in \code{target}.
+#' @param target The object of class \code{\linkS4class{VegX}} to be modified.
+#' @param x A taxon by site (plot) table (with plots in rows and taxa in columns) and with plant abundance values.
 #' @param abundanceMethod A measurement method for aggregate plant abundance (an object of class \code{\linkS4class{VegXMethodDefinition}}).
-#' @param obsDates A vector of \code{\link{Date}} objects with plot observation dates.
+#' @param obsDates A \code{\link{Date}} or a vector of \code{\link{Date}} objects with plot observation dates. 
+#' @param isTaxon A boolean flag (or vector) to indicate which organism names are taxa.
 #' @param absence.values A vector of values to be interpreted as missing plant information.
 #' @param verbose A flag to indicate console output of the data integration process.
 #'
@@ -22,23 +17,14 @@
 #'
 addTaxonBySiteData <-function(target,
                               x,
-                              projectTitle,
                               abundanceMethod,
-                              obsDates = Sys.Date(), absence.values = c(NA, 0),
+                              obsDates = Sys.Date(), 
+                              isTaxon = TRUE,
+                              absence.values = c(NA, 0),
                               verbose = TRUE) {
 
   if(class(target)!="VegX") stop("Wrong class for 'target'. Should be an object of class 'VegX'")
   
-  #get project ID and add new project if necessary
-  nprid = .newProjectIDByTitle(target,projectTitle)
-  projectID = nprid$id
-  if(nprid$new) {
-    target@projects[[projectID]] = list("title" = projectTitle)
-    if(verbose) cat(paste0(" New project '", projectTitle,"' added.\n"))
-  } else {
-    if(verbose) cat(paste0(" Data will be added to existing project '", projectTitle,"'.\n"))
-  }
-
   #plots
   orinplots = length(target@plots)
   nplot = nrow(x)
@@ -51,7 +37,7 @@ addTaxonBySiteData <-function(target,
   }
   finnplots = length(target@plots)
   if(verbose) {
-    cat(paste0(" ", finnplots-orinplots, " new plots added.\n"))
+    cat(paste0(" ", finnplots-orinplots, " new plot(s) added.\n"))
   }
 
   #plot observations
@@ -64,29 +50,35 @@ addTaxonBySiteData <-function(target,
     plotObsIDs[i] = npoid$id
     if(npoid$new) {
       target@plotObservations[[plotObsIDs[i]]] = list("plotID" = plotIDs[i],
-                                                      "projectID" = projectID,
                                                       "obsStartDate" = obsDates[i])
     }
   }
   finnpobs = length(target@plotObservations)
   if(verbose) {
-    cat(paste0(" ", finnpobs-orinpobs, " new plot observations added.\n"))
+    cat(paste0(" ", finnpobs-orinpobs, " new plot observation(s) added.\n"))
   }
 
 
-  # #taxon name usage concepts
-  orintuc = length(target@taxonNameUsageConcepts)
-  tnucNames = colnames(x)
-  ntnuc = length(tnucNames)
-  tnucIDs = character(0)
-  for(i in 1:ntnuc) {
-    ntnucid = .newTaxonNameUsageConceptIDByName(target, tnucNames[i]) # Get the new taxon name usage ID (internal code)
-    tnucIDs[i] = ntnucid$id
-    if(ntnucid$new) target@taxonNameUsageConcepts[[tnucIDs[i]]] = list("authorTaxonName" = tnucNames[i])
+  # organism identities/organism names
+  orinon = length(target@organismNames)
+  orinoi = length(target@organismIdentities)
+  orgNames = colnames(x)
+  noid = length(orgNames)
+  if(length(isTaxon)==1) isTaxon = rep(isTaxon, noid)
+  orgIDs = character(0)
+  for(i in 1:noid) {
+    nonid = .newOrganismNameIDByName(target, orgNames[i], isTaxon[i]) # Get the new taxon name usage ID (internal code)
+    if(nonid$new) target@organismNames[[nonid$id]] = list("name" = orgNames[i],
+                                                         "taxon" = isTaxon[i])
+    noiid = .newOrganismIdentityIDByTaxonConcept(target, orgNames[i], "") # Get the new taxon name usage ID (internal code)
+    orgIDs[i] = noiid$id
+    if(noiid$new) target@organismIdentities[[noiid$id]] = list("originalOrganismNameID" = nonid$id)
   }
-  finntuc = length(target@taxonNameUsageConcepts)
+  finnon = length(target@organismNames)
+  finnoi = length(target@organismIdentities)
   if(verbose) {
-    cat(paste0(" ", finntuc-orintuc, " new taxon name usage concepts added.\n"))
+    cat(paste0(" ", finnon-orinon, " new organism name(s) added.\n"))
+    cat(paste0(" ", finnoi-orinoi, " new organism identity(ies) added.\n"))
   }
 
   #methods/attributes (WARNING: method match should be made by attributes?)
@@ -99,11 +91,11 @@ addTaxonBySiteData <-function(target,
                                       attributeType = abundanceMethod@attributeType)
     if(verbose) cat(paste0(" Abundance measurement method '", abundanceMethod@name,"' added.\n"))
     # add literature citation if necessary
-    if(method@citationString!="") {
-      ncitid = .newLiteratureCitationIDByCitationString(target, method@citationString)
+    if(abundanceMethod@citationString!="") {
+      ncitid = .newLiteratureCitationIDByCitationString(target, abundanceMethod@citationString)
       if(ncitid$new) {
-        target@literatureCitations[[ncitid$id]] = list(citationString =method@citationString)
-        if(method@DOI!="")  target@literatureCitations[[ncitid$id]]$DOI = method@DOI
+        target@literatureCitations[[ncitid$id]] = list(citationString =abundanceMethod@citationString)
+        if(abundanceMethod@DOI!="")  target@literatureCitations[[ncitid$id]]$DOI = abundanceMethod@DOI
       }
       target@methods[[methodID]]$citationID = ncitid$id
     }
@@ -127,10 +119,21 @@ addTaxonBySiteData <-function(target,
   # aggregate organism observations
   absence.values = as.character(absence.values)
   orinaoo = length(target@aggregateObservations)
-  aggObsCounter = orinaoo+1 #counter
   for(i in 1:nplot) {
-    for(j in 1:ntnuc) {
+    for(j in 1:noid) {
       if(!(as.character(x[i,j]) %in% absence.values)) {
+        naoID = .newAggregateOrganismObservationIDByOrganismIdentityID(target, 
+                                                                       plotObsIDs[i], 
+                                                                       "", orgIDs[j])
+        aggObsID = naoID$id
+        if(naoID$new) {
+          aggObs = list("plotObservationID" = plotObsIDs[i],
+                        "organismIdentityID" = orgIDs[j],
+                        "stratumObservationID" = "")
+        }
+        else {
+          aggObs = target@aggregateObservations[[aggObsID]]
+        }
         if(abundanceMethod@attributeType== "quantitative") {
           attID = "1"
           if(x[i,j]> abundanceMethod@attributes[[1]]$upperLimit) {
@@ -144,18 +147,15 @@ addTaxonBySiteData <-function(target,
           if(length(ind)==1) attID = ids[ind]
           else stop(paste0("Value '", x[i,j],"' not found in measurement definition. Please revise scale or data."))
         }
-        target@aggregateObservations[[as.character(aggObsCounter)]] = list("plotObservationID" = plotObsIDs[i],
-                                        "taxonNameUsageConceptID" = tnucIDs[j],
-                                        "stratumObservationID" = "",
-                                        "attributeID" = attID,
-                                        "value" = x[i,j])
-        aggObsCounter = aggObsCounter + 1
+        aggObs$aggregateOrganismMeasurements = list()
+        aggObs$aggregateOrganismMeasurements[[1]] = list("attributeID" = attID,"value" = x[i,j])
+        target@aggregateObservations[[aggObsID]] = aggObs
       }
     }
   }
   finnaoo = length(target@aggregateObservations)
   if(verbose) {
-    cat(paste0(" ", finnaoo-orinaoo, " new aggregate organism observations added.\n"))
+    cat(paste0(" ", finnaoo-orinaoo, " new aggregate organism observation(s) added.\n"))
   }
 
   return(target)
