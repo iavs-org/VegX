d027178f762c3053d30e2499e358c2655804d8cc
LimaRAF <raflima@usp.br>
2022-04-27 10:13:04 +0200

    Adding new predefined quantitative, qualitative and ordinal methods, along with their descriptions

--- R/documentationMethods.r
+++ R/documentationMethods.r
@@ -1,9 +1,11 @@
-#' List of predefined quantitative methods
+#' @title List of predefined quantitative methods
 #'
-#' This data frame contains the definition of predefined quantitative methods available for Veg-X. 
-#' These are used by calling function \code{\link{predefinedMeasurementMethod}} or directly by their name. 
-#' When a string refers to a subject, the default method for that subject is used.
-#'
+#' @description This data frame contains the definition of predefined quantitative methods
+#' available for Veg-X. These are used by calling function
+#' \code{\link{predefinedMeasurementMethod}} or directly by their name. When a
+#' string refers to a subject, the default method for that subject is used.
+#' 
+#' The main columns contained in this data frame are:
 #' \itemize{
 #'  \item subject The description of an attribute class for comparative purposes.
 #'  \item name The name of the measurement method (normally includes units)
@@ -28,3 +30,64 @@
 #' predefinedMeasurementMethod("Plant cover/%")
 NULL
 
+#' @title List of predefined qualitative methods
+#'
+#' @description  This list contains the definition of predefined qualitative
+#'   methods available for Veg-X.
+#' 
+#' The main elements contained in this list of methods are:
+#' \itemize{
+#'  \item name The name of the method
+#'  \item description Short description of the method
+#'  \item subject The description of an attribute class for comparative purposes.
+#'  \item citationString The bibliographic reference containing the method
+#'  description (if available)
+#'  \item code The class codes of the qualitative variable
+#'  \item definition The definitions of the class codes of the qualitative variable
+#' }
+#'
+#' @name qualitative_methods
+#' @aliases qualitative_methods
+#' @docType data
+#' @author Veg-X team.
+#' @keywords data
+#' @seealso \code{\link{defineQualitativeScaleMethod}}
+#' @examples
+#' 
+#' # To explore available methods
+#' names(qualitative_methods)
+#' 
+NULL
+
+#' @title List of predefined ordinal methods
+#'
+#' @description  This list contains the definition of predefined ordinal methods
+#'   available for Veg-X.
+#' 
+#' The main elements contained in this list of methods are:
+#' \itemize{
+#'  \item name The name of the method
+#'  \item description Short description of the method
+#'  \item subject The description of an attribute class for comparative
+#'  purposes.
+#'  \item citationString The bibliographic reference containing the method
+#'  description (if available)
+#'  \item code The ordinal codes of the ordinal variable
+#'  \item definition The definitions of the codes of the ordinal variable
+#'  \item lowerLimit, upperLimit and midPoint The limits and midpoint of the
+#'  ordinal codes
+#'  
+#' }
+#'
+#' @name ordinal_methods
+#' @aliases ordinal_methods
+#' @docType data
+#' @author Veg-X team.
+#' @keywords data
+#' @seealso \code{\link{defineOrdinalScaleMethod}}
+#' @examples
+#' 
+#' # To explore available methods
+#' names(ordinal_methods)
+#' 
+NULL
---
+++ R/documentationObjects.r
@@ -0,0 +1,42 @@
+#' Objects to Support Data Mapping 
+#' 
+#' @description 
+#' A named list of different objects used to support the process of mapping the
+#' names of fields and methods into the internal standard required by the
+#' *VegX* function `prepareMapping()` (see the function help for other
+#' details). 
+#' 
+#' Only the networks or databases that agreed to provide the equivalencies of
+#' their datasets to the standards required are included here. If you want to
+#' provide a map or method to be included as a default map within *VegX*,
+#' please let us know \href{https://github.com/iavs-org/VegX/issues}{here}.
+#' 
+#' @details 
+#' The list of supporting objects is:
+#' 
+#' \itemize{
+#'
+#' \item{`reference_map`: a named list of empty vectors which works as a
+#' reference for all fields included in the internal standard.}
+#'
+#' \item{`predefined_fields`: a vector of field names that are potentially
+#' present in the internal file containig the standard used for mapping.}
+#'
+#' \item{`available_projects`: the list of selected networks or databases with
+#' predefined mapping currently available in *VegX*.}
+#'
+#' \item{`predefined_map`: a data frame containing the predefined mapping
+#' itself.}
+#'
+#' \item{`available_methods`: a vector of predefined method names that are
+#' currently available in *VegX*.}
+#'
+#' }
+#' 
+#' @name supporting_info
+#' @docType data
+#' @keywords data
+#' @format A Named List
+#' @source \url{INCLUDELINKHERE}
+"supporting_info"
+
